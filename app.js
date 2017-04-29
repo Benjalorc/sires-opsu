0f64ba1ab666f7cb574937430f707abbbaa74477
benjamin_s_e <benjalorc@gmail.com>
2017-04-29 00:24:01 +0000

    ---configuracion inicial 1-- Inicio del proyecto en node.js con el comando npm init 2-- Configuracion del archivo package.json 	2.1--Se creo la lista de dependencias requeridas 	2.2--Se configuro el script de arranque 3-- Se crea el archivo app.js en la ruta / 	3.1-- Se importaron los paquetes principales 	3.2-- Se configuro el puerto del servidor 	3.3-- Se definio el uso de los middlewares cors y body-parser 	3.4-- Se establecio el enrutamiento para /users 	3.5-- Se definio la ubicacion del static folder para la aplicacion principal en /client 	3.6-- Se definio la ruta index en / 	3.7-- Se establecio la conexion a la base de datos mongoDB con mongoose 4-- Se crea el archivo users.js en /routes 	4.1-- Se definieron las respuestas para varias rutas correspondientes a los usuarios 5-- Se creo el archivo database.js en la ruta /config para definir el objeto de conexion a la base de datos 6-- Se creo el archivo index.html en la ruta /client con un esqueleto HTML en blanco

---
+++ app.js
@@ -0,0 +1,54 @@
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const passport = require('passport');
+const mongoose = require('mongoose');
+const config = require('./config/database')
+
+const app = express();
+
+//CONNECT TO DATABASE
+mongoose.connect(config.database);
+
+//ON CONNECTION
+mongoose.connection.on('connected', () => {
+   
+   console.log('Conectado a la base de datos mongodb: '+config.database);
+});
+
+//ON ERROR
+mongoose.connection.on('error', (err) => {
+   
+   console.log('Database error: '+err);
+});
+const users = require('./routes/users')
+
+//PORT NUMBER
+const port = process.env.PORT;
+
+//CORS MIDDLEWARE
+app.use(cors());
+
+//BODY-PARSER MIDDLEWARE
+app.use(bodyParser.json());
+
+
+
+//Respondera al llamar a /users/xxxxxx
+app.use('/users', users)
+
+//STATIC FOLDER
+app.use(express.static(path.join(__dirname, 'client')));
+
+//INDEX ROUTE
+app.get('/', (req, res) => {
+    res.send("Hola a todos!")
+});
+
+
+//START SERVER
+app.listen(port, () => {
+    
+    console.log("Servidor iniciado en el puerto "+port);
+});
---
+++ routes/users.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const router = express.Router();
+
+//REGISTER
+
+router.get('/register', (req, res, next) => {
+    res.send('REGISTER');
+});
+
+//AUNTHENTICATE
+router.post('/authenticate', (req, res, next) => {
+    res.send('AUNTHENTICATE');
+});
+
+//VALIDATE
+router.get('/validate', (req, res, next) => {
+    res.send('PROFILE');
+});
+
+//PROFILE
+router.get('/profile', (req, res, next) => {
+    res.send('PROFILE');
+});
+
+module.exports = router;

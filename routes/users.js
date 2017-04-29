22ae0686b19d6d6b86d2603559ab67505e43efd5
benjamin_s_e <benjalorc@gmail.com>
2017-04-29 14:12:38 +0000

    Implementado el comportamiento en backend para añadir nuevos usuarios y registrarlos en la base de datos

--- routes/users.js
+++ routes/users.js
@@ -1,19 +1,38 @@
 const express = require('express');
 const router = express.Router();
+const passport = require('passport');
+const jwt = require('jsonwebtoken');
+
+const User = require('../models/user')
 
 //REGISTER
-router.get('/register', (req, res, next) => {
-    res.send('REGISTER');
+router.post('/register', (req, res, next) => {
+    //res.send('REGISTER');
+    'use strict';
+    let newUser = new User({
+        cedula: req.body.cedula,
+        nombre: req.body.nombre,
+        apellido: req.body.apellido,
+        username: req.body.username,
+        password: req.body.password,
+        f_nac: req.body.f_nac,
+        sexo: req.body.sexo
+    });
+    
+    User.addUser(newUser, (err, user) =>{
+        
+        if(err){
+            res.json({success:false, msg:"Fallo al registrar usuario"});
+        }else{
+            res.json({success:true, msg:"Usuario registrado"});
+        }
+    });
+    
 });
 
 //AUNTHENTICATE
 router.post('/authenticate', (req, res, next) => {
     res.send('AUNTHENTICATE');
-});
-
-//VALIDATE
-router.get('/validate', (req, res, next) => {
-    res.send('PROFILE');
 });
 
 //PROFILE

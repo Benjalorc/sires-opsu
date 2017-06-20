c69c89efb7083c04714ba82d2bbba19e766d58bf
benjamin_s_e <benjalorc@gmail.com>
2017-06-20 04:26:03 +0000

    Creado el direccionamiento al panel de administradores

--- angular-src/src/app/app.module.ts
+++ angular-src/src/app/app.module.ts
@@ -12,6 +12,7 @@ import { RegisterComponent } from './components/register/register.component';
 import { HomeComponent } from './components/home/home.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { ProfileComponent } from './components/profile/profile.component';
+import { SysadminComponent } from './components/sysadmin/sysadmin.component';
 
 
 import { ValidateService } from './services/validate/validate.service';
@@ -24,7 +25,8 @@ const appRoutes : Routes = [
   { path: 'register', component: RegisterComponent },
   { path: 'login', component: LoginComponent },
   { path: 'dashboard', component: DashboardComponent, canActivate:[AuthGuard] },
-  { path: 'profile', component: ProfileComponent, canActivate:[AuthGuard]}
+  { path: 'profile', component: ProfileComponent, canActivate:[AuthGuard]},
+  { path: 'admin', component: SysadminComponent, canActivate:[AuthGuard]}
 ]
 
 @NgModule({
@@ -35,7 +37,8 @@ const appRoutes : Routes = [
     RegisterComponent,
     HomeComponent,
     DashboardComponent,
-    ProfileComponent
+    ProfileComponent,
+    SysadminComponent
   ],
   imports: [
     BrowserModule,

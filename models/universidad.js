0a476e89fb4b4adf7aada28594b773af908ae7bd
benjamin_s_e <benjalorc@gmail.com>
2017-07-06 11:52:46 +0000

    Construccion de la clase universidad y sus metodos, con repoblado de datos de municipio y carreras

---
+++ models/universidad.js
@@ -0,0 +1,67 @@
+const mongoose = require('mongoose');
+const config = require('../config/database');
+
+//Universidad Schema
+const UniversidadSchema = mongoose.Schema({
+    
+    codigo:{
+        type: String,
+        required: true
+    },
+    nombre:{
+        type: String,
+        required: true
+    },
+    municipio:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Municipio',
+        required: true
+    },
+    capacidad:{
+        type: Number,
+        required: true
+    },
+    poblacion:{
+        type: Number,
+        required: true
+    },
+    carreras:[{
+        type: mongoose.Schema.Types.ObjectId, 
+        ref: "Carrera",
+        required: false
+    }]
+
+});
+
+const Universidad = module.exports = mongoose.model('Universidad', UniversidadSchema, 'universidades');
+
+module.exports.registrarUniversidad = function(universidad, callback){
+    universidad.save(callback);
+}
+
+module.exports.buscarUniversidad = function(data, callback){
+
+    const query = {codigo: data};
+    Universidad.findOne(query)
+        .populate("carreras")
+        .exec(callback);
+}
+
+module.exports.modificarUniversidad = function(data, callback){
+    
+    const query = {codigo: data.codigo};
+    Universidad.findOneAndUpdate(query, { $set: data.updatedData}, callback);
+}
+
+module.exports.incluirCarrera = function(data, callback){
+    
+    const query = {codigo: data.codigo};
+    //Universidad.findOneAndUpdate(query, { $push: data.updatedData}, callback);
+    Universidad.findOne(query, callback)
+}
+
+module.exports.eliminarUniversidad = function(data, callback){
+    
+    const query = {codigo: data};
+    Universidad.findOneAndRemove(query, callback);
+}
---
+++ routes/universidades.js
@@ -0,0 +1,151 @@
+const express = require('express');
+const router = express.Router();
+const Universidad = require('../models/universidad');
+const Municipio = require('../models/municipio');
+const Carrera = require('../models/carrera');
+
+
+//REGISTRAR UNIVERSIDAD
+router.post('/registrar', (req, res, next) =>{
+    'use strict';
+
+    let cod_mun = req.body.municipio;
+    Municipio.buscarMunicipio(cod_mun, (err, municipio) =>{
+        
+        if(err){
+            return res.json({success: false, msg: "Error al consultar en municipio"});
+        }
+        
+        if(municipio){
+            
+            let newUniversidad = new Universidad({
+                codigo: req.body.codigo,
+                nombre: req.body.nombre,
+                municipio: municipio._id,
+                capacidad: req.body.capacidad,
+                poblacion: req.body.poblacion
+            });
+
+            console.log(newUniversidad);
+            Universidad.registrarUniversidad(newUniversidad, (err, universidad) =>{
+        
+                if(err){
+                    return res.json({success:false, msg:"Fallo al agregar universidad en la base de datos"});
+                }else{
+                    return res.json({success:true, msg:"Universidad agregada con exito"});
+                }
+            });
+        }else{
+            return res.json({success: false, msg: "No se encontro el municipio"})
+        }
+    })
+    
+});
+    
+router.get('/buscar/:codigo', (req, res, next) =>{
+    'use strict';
+
+    let codigo = req.params.codigo;
+
+    Universidad.buscarUniversidad(codigo, (err, universidad) =>{
+        
+        
+        if(err){
+            return res.json({success: false, msg: "Error al ejecutar la consulta"});
+        }
+        
+        if(universidad){
+            return res.json({success: true, msg: "Se encontro el municipio", data: universidad});
+        }else{
+            return res.json({success: false, msg: "Municipio no encontrado"});
+        }
+    });
+});
+
+router.put('/incluircarrera', (req, res, next) =>{
+    
+    let carr_cod = req.body.carrera;
+    Carrera.buscarCarrera(carr_cod, (err, carrera) =>{
+        
+        if(err){
+            return res.json({success: false, msg: "Error al consultar en carrera"});
+        }
+        
+        if(carrera){
+            
+            let universidad = {
+                codigo: req.body.codigo,
+                updatedData: carrera._id
+            }
+            
+            console.log(universidad);
+            Universidad.incluirCarrera(universidad, (err, uni) =>{
+               
+                if(err){
+                   return res.json({success: false, msg: "Error al ejecutar la consulta"});
+                }
+               
+                if(uni){
+                    uni.carreras.push(universidad.updatedData);
+                    uni.save(function(err, newUni){
+                        
+                        if(err){
+                            return res.json({success: false, msg: "No se pudo incluir la carrera"});
+                        }else{
+                            return res.json({success: true, msg: "Todo parece estar en orden"});
+                        }
+                    });
+                }else{
+                    return res.json({success: false, msg: "La carrera no pudo incluirse"});
+                }
+            });            
+        }else{
+            return res.json({success: false, msg: "No se encontro la carrera"});
+        }
+    });
+});
+
+
+
+router.put('/actualizar', (req, res, next) =>{
+    'use strict'; 
+    
+    let universidad = {
+        codigo: req.body.codigo,
+        updatedData: req.body.datosNuevos
+    }
+    
+    Universidad.actualizarUniversidad(universidad, (err, uni) =>{
+       
+        if(err){
+           return res.json({success: false, msg: "Error al ejecutar la consulta"});
+        }
+       
+        if(uni){
+            return res.json({success: true, msg: "Se actualizo la universidad"});
+        }else{
+            return res.json({success: false, msg: "La universidad no pudo actualizarse"});
+        }
+    });
+});
+
+router.delete('/eliminar', (req, res, next) =>{
+    'use strict';
+
+    let uni_cod = req.body.codigo;
+    
+    Universidad.eliminarUniversidad(uni_cod, (err, uni) =>{
+        
+        if(err){
+           return res.json({success: false, msg: "Error al ejecutar la consulta"});
+        }
+       
+        if(uni){
+            return res.json({success: true, msg: "Se elimino la universidad"});
+        }else{
+            return res.json({success: false, msg: "La universidad no pudo ser eliminada"});
+        }
+    });
+});
+
+module.exports = router;

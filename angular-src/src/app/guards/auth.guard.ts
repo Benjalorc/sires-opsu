f13f462c08ea9733ee31c639392971261ce791f2
benjamin_s_e <benjalorc@gmail.com>
2017-10-07 20:25:01 +0000

    Mejoras en el servicio de autenticacion en redireccionamiento

--- angular-src/src/app/guards/auth.guard.ts
+++ angular-src/src/app/guards/auth.guard.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { CanActivate, Router } from '@angular/router';
 import { AuthService} from '../services/auth/auth.service';
+import { ActivatedRouteSnapshot } from '@angular/router';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
@@ -11,14 +12,26 @@ export class AuthGuard implements CanActivate {
     ){
   }
   
-  canActivate(){
+  canActivate(route: ActivatedRouteSnapshot){
     
-    if(this.authService.loggedIn()){
+    if(this.authService.loggedIn() && (route.data['ruta'] == 'login' || route.data['ruta'] == 'register')){
+      this.router.navigate(['/dashboard']);
+      return false;
+    }
+    else if(!this.authService.loggedIn() && (route.data['ruta'] == 'login' || route.data['ruta'] == 'register')){
       return true;
-    }else{
+    }
+    else if(this.authService.loggedIn() && (route.data['ruta'] == 'dashboard' || route.data['ruta'] == 'profile' || route.data['ruta'] == 'admin' )){
+      return true;
+    }
+    else if(!this.authService.loggedIn() && (route.data['ruta'] == 'dashboard' || route.data['ruta'] == 'profile' || route.data['ruta'] == 'admin' )){
       this.router.navigate(['/login']);
       return false;
     }
+    else{
+      return true;
+    }
     
   }
+
 }

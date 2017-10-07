d0b280cdec9c4c64b250f85a8ff3ffe16f61ac05
benjamin_s_e <benjalorc@gmail.com>
2017-10-07 19:45:32 +0000

    Se ocultaron algunos botones para usuarios con sesion iniciada

--- angular-src/src/app/components/home/home.component.ts
+++ angular-src/src/app/components/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FlashMessagesService } from 'angular2-flash-messages';
+import { AuthService} from '../../services/auth/auth.service';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +9,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  loggedIn: Boolean;
+
+  constructor(public authService: AuthService,
+              private flashMessage : FlashMessagesService) { }
 
   ngOnInit() {
+    
+    if(this.authService.loggedIn()){
+      this.loggedIn = true;
+    }
   }
 
 }

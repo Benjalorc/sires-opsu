34371a6c65dfaed00fd003550c0fab8ce252835e
Benjalorc <benjalorc@gmail.com>
2017-10-20 20:59:03 -0400

    Inclusion de componentes nuevos en modulo principal

--- angular-src/src/app/app.module.ts
+++ angular-src/src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
@@ -14,12 +15,17 @@ import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { ProfileComponent } from './components/profile/profile.component';
 import { SysadminComponent } from './components/sysadmin/sysadmin.component';
 import { AdminUnivComponent } from './components/sysadmin/admin-univ/admin-univ.component';
+import { AdminCarrComponent } from './components/sysadmin/admin-carr/admin-carr.component';
+import { AdminIndComponent } from './components/sysadmin/admin-ind/admin-ind.component';
 
 import { ValidateService } from './services/validate/validate.service';
 import { AuthService } from './services/auth/auth.service';
 import { UniversidadesService } from './services/universidades/universidades.service';
 import { MunicipiosService } from './services/municipios/municipios.service';
 import { CarrerasService } from './services/carreras/carreras.service';
+import { PnevsService } from './services/pnevs/pnevs.service';
+import { SnisService } from './services/snis/snis.service';
+import { EstudiantesService } from './services/estudiantes/estudiantes.service';
 import { AuthGuard } from './guards/auth.guard';
 import { MyDatePickerModule } from 'mydatepicker';
 
@@ -30,7 +36,9 @@ const appRoutes : Routes = [
   { path: 'dashboard', component: DashboardComponent, canActivate:[AuthGuard], data:{ ruta: 'dashboard' } },
   { path: 'profile', component: ProfileComponent, canActivate:[AuthGuard], data:{ ruta: 'profile' } },
   { path: 'admin', component: SysadminComponent, canActivate:[AuthGuard], data:{ ruta: 'admin' } },
-  { path: 'admin/universidades', component: AdminUnivComponent, canActivate:[AuthGuard], data:{ ruta: 'universidades' } }
+  { path: 'admin/universidades', component: AdminUnivComponent, canActivate:[AuthGuard], data:{ ruta: 'universidades' } },
+  { path: 'admin/carreras', component: AdminCarrComponent, canActivate:[AuthGuard], data:{ ruta: 'carreras' } },
+  { path: 'admin/indicadores', component: AdminIndComponent, canActivate:[AuthGuard], data:{ ruta: 'indicadores' } }
 ]
 
 @NgModule({
@@ -43,17 +51,20 @@ const appRoutes : Routes = [
     DashboardComponent,
     ProfileComponent,
     SysadminComponent,
-    AdminUnivComponent
+    AdminUnivComponent,
+    AdminCarrComponent,
+    AdminIndComponent
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     MyDatePickerModule,
     FormsModule,
     HttpModule,
     RouterModule.forRoot(appRoutes),
     FlashMessagesModule
   ],
-  providers: [ValidateService, AuthService, AuthGuard, UniversidadesService, MunicipiosService, CarrerasService],
+  providers: [ValidateService, AuthService, AuthGuard, EstudiantesService, UniversidadesService, PnevsService, SnisService, MunicipiosService, CarrerasService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }

c878c213d1d99f0fa724a45cb20326b0f14f324d
benjamin_s_e <benjalorc@gmail.com>
2017-10-22 17:43:14 +0000

    Se incluyo el servicio para llamada al API de registro de estudiantes

--- angular-src/src/app/services/estudiantes/estudiantes.service.ts
+++ angular-src/src/app/services/estudiantes/estudiantes.service.ts
@@ -8,9 +8,32 @@ export class EstudiantesService {
 
   constructor(private http: Http) { }
   
+    validateStudent(estudiante){
+      'use strict'
+      let avaible = Object.getOwnPropertyNames(estudiante);
+      let pass = true;
+  
+      avaible.forEach((element) =>{
+        if(estudiante[element] == undefined || estudiante[element] == '' || estudiante[element] == null){
+          pass = false
+        }
+        else{
+        }
+      });
+      return pass;
+    }
+    
     getStudents(){
         'use strict'
         return this.http.get('http://localhost:8080/estudiantes/all')
+            .map(res => res.json());
+    }
+    
+    registrarEstudiante(estudiante){
+      'use strict'
+      let headers = new Headers();
+      headers.append('Content-Type','application/json');
+      return this.http.post('http://localhost:8080/estudiantes/registrar', estudiante, {headers: headers})
             .map(res => res.json());
     }
 

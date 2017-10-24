ea8068aac693332c22f50b6831be1b988a39e1db
benjalorc <benjalorc@gmail.com>
2017-10-24 19:25:53 +0000

    Se elimino el codigo de la libreria chartjs en prueba para implementar en un modulo independiente

--- angular-src/src/app/components/sysadmin/sysadmin.component.ts
+++ angular-src/src/app/components/sysadmin/sysadmin.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import Chart from 'chart.js';
 import { trigger,state,style,transition,animate,keyframes } from '@angular/animations';
 
 
@@ -216,7 +215,6 @@ export class SysadminComponent implements OnInit {
     this.icon6 = 'b0';
     this.icon7 = 'b0';
     this.icon8 = 'b0';
-
   }
 
   animateLeft(){
@@ -242,78 +240,5 @@ export class SysadminComponent implements OnInit {
     this.icon1 = 'a0';
     this.icon2 = 'a0';
   }
-
-
-  firstChart(){
-    let ctx = "myChart";
-    
-    let chart = new Chart(ctx, {
-        // The type of chart we want to create
-        type: 'line',
-
-        // The data for our dataset
-        data: {
-            labels: ["January", "February", "March", "April", "May", "June", "July"],
-            datasets: [{
-                label: "My First dataset",
-                backgroundColor: 'rgb(255, 99, 132)',
-                borderColor: 'rgb(255, 99, 132)',
-                data: [0, 10, 5, 2, 20, 30, 45],
-            }]
-        },
-
-        // Configuration options go here
-        options: {
-          tooltips: {
-            mode: 'point'
-          }
-        }
-    });
-  }
-
-  secondChart(){
-    let ctx = "myChart"
-    let chart = new Chart(ctx, {
-      type: 'bar',
-      data: {
-          labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
-          datasets: [{
-              label: '# of Votes',
-              data: [12, 19, 3, 5, 2, 3],
-              backgroundColor: [
-                  'rgba(255, 99, 132, 0.2)',
-                  'rgba(54, 162, 235, 0.2)',
-                  'rgba(255, 206, 86, 0.2)',
-                  'rgba(75, 192, 192, 0.2)',
-                  'rgba(153, 102, 255, 0.2)',
-                  'rgba(255, 159, 64, 0.2)'
-              ],
-              borderColor: [
-                  'rgba(255,99,132,1)',
-                  'rgba(54, 162, 235, 1)',
-                  'rgba(255, 206, 86, 1)',
-                  'rgba(75, 192, 192, 1)',
-                  'rgba(153, 102, 255, 1)',
-                  'rgba(255, 159, 64, 1)'
-              ],
-              borderWidth: 1
-          }]
-      },
-      options: {
-        scales: {
-          yAxes: [{
-              ticks: {
-                beginAtZero:true
-              }
-          }]
-        }
-      }
-    });
-  }
-
-
-
-
-
 
 }
